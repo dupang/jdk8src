435156bc511e7758e6d8b00858f6f279352aae06
dupang <372850928@qq.com>
2016-05-19 19:31:00 +0800

    commit

--- java/util/concurrent/locks/Lock.java
+++ java/util/concurrent/locks/Lock.java
@@ -43,6 +43,9 @@ import java.util.concurrent.TimeUnit;
  * quite different properties, and may support multiple associated
  * {@link Condition} objects.
  *
+ * Lock实现提供了比使用synchronized方法和synchronized语句块扩展性更好的锁操作。
+ * 他们允许更灵活地构建，可能有相当不同的属性，并且可以支持多个相关的Condition对象。
+ *
  * <p>A lock is a tool for controlling access to a shared resource by
  * multiple threads. Commonly, a lock provides exclusive access to a
  * shared resource: only one thread at a time can acquire the lock and
@@ -50,12 +53,19 @@ import java.util.concurrent.TimeUnit;
  * acquired first. However, some locks may allow concurrent access to
  * a shared resource, such as the read lock of a {@link ReadWriteLock}.
  *
+ * 锁是一个控制被多个线程共享的资源访问的工具。一般来说，锁对共享资源提供排它地访问:
+ * 只能一个时间内只能一个线程可以获取锁并且所有对共享资源的访问必需先获取锁。然而
+ * 一些锁可以允许对共享的资源并发访问，比如ReadWriteLock的读锁。
+ *
  * <p>The use of {@code synchronized} methods or statements provides
  * access to the implicit monitor lock associated with every object, but
  * forces all lock acquisition and release to occur in a block-structured way:
  * when multiple locks are acquired they must be released in the opposite
  * order, and all locks must be released in the same lexical scope in which
  * they were acquired.
+ *
+ * 同步访法或同步块提供用每一个对象来访问隐含的监控锁，但是强制所有锁获取和释放都以块结构的方式:
+ * 当多个锁被获取，他们必需以和获取顺序相反的顺序释放，并且所有锁必需被释放在他们获取的作用域中。
  *
  * <p>While the scoping mechanism for {@code synchronized} methods
  * and statements makes it much easier to program with monitor locks,
@@ -71,11 +81,20 @@ import java.util.concurrent.TimeUnit;
  * and allowing multiple locks to be acquired and released in any
  * order.
  *
+ * 尽管同步方法和同步块的作用域机制使它编写监视锁更容易，并且帮助避免很多有关锁的普通
+ * 编程错误，有时候你需要以一种更灵活的方式和锁打交道。例如，遍历并发访问的数据结构的算法
+ * 需要使用hand-over-hand或者链式锁：你获取节点A的锁，然后节点B,然后释放A并且获取C,
+ * 然后释放B并且获取D等等。Lock接口的实现使这种技术通过锁的获取和释放用不同的作用域来使用，
+ * 并且允许多个锁以任何顺序获取和释放。
+ *
  * <p>With this increased flexibility comes additional
  * responsibility. The absence of block-structured locking removes the
  * automatic release of locks that occurs with {@code synchronized}
  * methods and statements. In most cases, the following idiom
  * should be used:
+ *
+ * 增加的灵活性带来的是额外的职责。没有了同步方法和同步块中的结构的获取和释放。
+ * 大部分情况，应该使用下面的风格。
  *
  *  <pre> {@code
  * Lock l = ...;
@@ -91,6 +110,9 @@ import java.util.concurrent.TimeUnit;
  * held is protected by try-finally or try-catch to ensure that the
  * lock is released when necessary.
  *
+ * 当锁和解锁出现在不同的作用域，必需加倍小心来保证当持有锁时被执行的代码被try-finally
+ * 或try-catch保护确保必要的时候锁被释放。
+ *
  * <p>{@code Lock} implementations provide additional functionality
  * over the use of {@code synchronized} methods and statements by
  * providing a non-blocking attempt to acquire a lock ({@link
@@ -98,11 +120,17 @@ import java.util.concurrent.TimeUnit;
  * interrupted ({@link #lockInterruptibly}, and an attempt to acquire
  * the lock that can timeout ({@link #tryLock(long, TimeUnit)}).
  *
+ * Lock实现通过提供一个非阻塞的获取锁的tryLock()方法来提供了比使用同步块和同步方法
+ * 额外的功能，试图获取锁的的操作可以被打断，并且试图获取锁的操作可以超时。
+ *
  * <p>A {@code Lock} class can also provide behavior and semantics
  * that is quite different from that of the implicit monitor lock,
  * such as guaranteed ordering, non-reentrant usage, or deadlock
  * detection. If an implementation provides such specialized semantics
  * then the implementation must document those semantics.
+ *
+ * 一个Lock类也可以提供和隐含的监视锁不同的等待和语法，例如顺序的保证，不可重入，或者
+ * 死锁检测。如果一个实现提供这些特别的语法，那么这个实现必须以文档地形式阐明这个语法。
  *
  * <p>Note that {@code Lock} instances are just normal objects and can
  * themselves be used as the target in a {@code synchronized} statement.
@@ -112,9 +140,15 @@ import java.util.concurrent.TimeUnit;
  * It is recommended that to avoid confusion you never use {@code Lock}
  * instances in this way, except within their own implementation.
  *
+ * 注意锁实例仅仅是一个普通对象，并且它们自己可以在同步块中被使用。获取Lock实例的监视锁
+ * 和调用Lock的任何方法没有特定的关系。为了避免混淆建议永远不要这样用Lock实例，
+ * 除非在他们自己的实现里。
+ *
  * <p>Except where noted, passing a {@code null} value for any
  * parameter will result in a {@link NullPointerException} being
  * thrown.
+ *
+ * 除了提醒过的，传一个null值给任何参数，将导致抛出NPE.
  *
  * <h3>Memory Synchronization</h3>
  *
@@ -130,11 +164,21 @@ import java.util.concurrent.TimeUnit;
  * memory synchronization effects as a successful <em>Unlock</em> action.
  * </ul>
  *
+ * 内存同步
+ *
+ * 所有Lock的实现必须执行和内置的监视锁一样的内存同步语义。就像The Java Language Specification中描述的一样。
+ * 一个成功的lock操作和Lock动作具有相同的内在语义;
+ * 一个成功的unlock操作和Unlock动作具有相同的内在语义;
+ *
  * Unsuccessful locking and unlocking operations, and reentrant
  * locking/unlocking operations, do not require any memory
  * synchronization effects.
  *
+ * 不成功的锁和解锁操作，和重入的锁/解锁操作，不需要任何内存同步效果。
+ *
  * <h3>Implementation Considerations</h3>
+ *
+ * 实现注意事项
  *
  * <p>The three forms of lock acquisition (interruptible,
  * non-interruptible, and timed) may differ in their performance
@@ -151,11 +195,21 @@ import java.util.concurrent.TimeUnit;
  * acquisition is supported: which is either totally, or only on
  * method entry.
  *
+ * 获取锁的三种形式(可中断的，不可中断的，和定时的)可能在性能特点，顺序保证，或者其它实现
+ * 上不同。而且，中断正在进行的获取锁操作可能在给定的Lock类没有提供。因此不需要为所有三种
+ * 形式的获取锁定义完全相同的保证或语义实现，也不需要支持中断正在进行的锁获取操作。实现需要
+ * 清晰地以文档地形式记录提供每一个锁方法的的语义和保证。这弛必须遵循接口中定义的中断语义，
+ * 在一定程度上支持获取锁的中断:完全支持或在进入方法的时候。
+ *
+ *
  * <p>As interruption generally implies cancellation, and checks for
  * interruption are often infrequent, an implementation can favor responding
  * to an interrupt over normal method return. This is true even if it can be
  * shown that the interrupt occurred after another action may have unblocked
  * the thread. An implementation should document this behavior.
+ *
+ * 由于中断通过意为着取消，并且检查中断是非常罕见的，比起普通返回方法，可以优先响应中断。
+ * 尽管可以被证明在另一个行为之后的中断可能不会阻塞这个线程，也应该这样做。实现应该记录这个行为。
  *
  * @see ReentrantLock
  * @see Condition
@@ -180,6 +234,9 @@ public interface Lock {
      * may throw an (unchecked) exception in such circumstances.  The
      * circumstances and the exception type must be documented by that
      * {@code Lock} implementation.
+     *
+     * 获取锁。
+     * 如果
      */
     void lock();
 
