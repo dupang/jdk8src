5d97c440fa15c1570dbbd2d86bbac8a254e8e5b7
dupang <372850928@qq.com>
2016-05-20 08:31:41 +0800

    own

--- java/util/concurrent/locks/AbstractOwnableSynchronizer.java
+++ java/util/concurrent/locks/AbstractOwnableSynchronizer.java
@@ -44,6 +44,10 @@ package java.util.concurrent.locks;
  * appropriately maintained values to help control and monitor access
  * and provide diagnostics.
  *
+ * 可以被线程独占地持有的同步器。这个类为创建锁和相关的同步器
+ * 可能包含所有权的概念提供一个基础。AbstractOwnableSynchronizer类本身
+ * 不管理或使用这个信息。但是子类和工具可以使用合适的值来帮助控制和
+ * 监视访问和提供诊断。
  * @since 1.6
  * @author Doug Lea
  */
@@ -51,15 +55,18 @@ public abstract class AbstractOwnableSynchronizer
     implements java.io.Serializable {
 
     /** Use serial ID even though all fields transient. */
+    //使用序列化ID 尽管所有的字段是临时的。
     private static final long serialVersionUID = 3737899427754241961L;
 
     /**
      * Empty constructor for use by subclasses.
+     * 子类使用的空的构造器
      */
     protected AbstractOwnableSynchronizer() { }
 
     /**
      * The current owner of exclusive mode synchronization.
+     * 独占模式同步器的当前拥有者
      */
     private transient Thread exclusiveOwnerThread;
 
@@ -81,6 +88,9 @@ public abstract class AbstractOwnableSynchronizer
      * Returns the thread last set by {@code setExclusiveOwnerThread},
      * or {@code null} if never set.  This method does not otherwise
      * impose any synchronization or {@code volatile} field accesses.
+     *
+     * 返回被setExclusiveOwnerThread方法最后设置的线程，或者null如果没有
+     * 设置。这个方法没有实施任何同步或voatile字段访问。
      * @return the owner thread
      */
     protected final Thread getExclusiveOwnerThread() {
