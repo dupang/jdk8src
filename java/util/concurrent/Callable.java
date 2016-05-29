78333f480d583d91b0e6127b084eee94c7f36b34
dupang <372850928@qq.com>
2016-05-29 22:27:41 +0800

    Callable

--- java/util/concurrent/Callable.java
+++ java/util/concurrent/Callable.java
@@ -40,14 +40,21 @@ package java.util.concurrent;
  * Implementors define a single method with no arguments called
  * {@code call}.
  *
+ * 返回一个结果并且可能抛出一个异常的任务。
+ *
  * <p>The {@code Callable} interface is similar to {@link
  * java.lang.Runnable}, in that both are designed for classes whose
  * instances are potentially executed by another thread.  A
  * {@code Runnable}, however, does not return a result and cannot
  * throw a checked exception.
  *
+ * Callable接口和Runnable接口很相似，他们的实例都是设计用来被其它线程执行的。
+ * 但是Runnable没有返回结果，并且不能抛出检查异常。
+ *
  * <p>The {@link Executors} class contains utility methods to
  * convert from other common forms to {@code Callable} classes.
+ *
+ * Executors 类包含工具方法来转换其它普通形式成Callable类。
  *
  * @see Executor
  * @since 1.5
