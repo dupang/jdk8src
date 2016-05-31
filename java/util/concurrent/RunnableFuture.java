034e04e3bec6be6330d26104ac90f9e661efae48
dupang <372850928@qq.com>
2016-05-31 08:40:24 +0800

    Executor

--- java/util/concurrent/RunnableFuture.java
+++ java/util/concurrent/RunnableFuture.java
@@ -39,6 +39,10 @@ package java.util.concurrent;
  * A {@link Future} that is {@link Runnable}. Successful execution of
  * the {@code run} method causes completion of the {@code Future}
  * and allows access to its results.
+ *
+ * Future是一个Runnable。成功执行的run方法使Future的完成并且允许它
+ * 访问它的结果。
+ *
  * @see FutureTask
  * @see Executor
  * @since 1.6
@@ -49,6 +53,9 @@ public interface RunnableFuture<V> extends Runnable, Future<V> {
     /**
      * Sets this Future to the result of its computation
      * unless it has been cancelled.
+     *
+     * 设置这个Future到这个计算结果。除非它被取消。
+     *
      */
     void run();
 }
